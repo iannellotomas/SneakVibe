8e58cfeae59e0150da270132df95124790629f7a
Tomás Iannello <iannello.mas@gmail.com>
2025-02-19 00:04:52 -0300

    Cart Context

--- src/helpers/asyncMock.js
+++ src/helpers/asyncMock.js
@@ -7,7 +7,7 @@ export const dataProducts = [
 			"Estas zapatillas deportivas están hechas con materiales reciclados, garantizando que al menos un 50% de su composición proviene de contenido reciclado. Con un diseño cómodo y funcional, son ideales para quienes buscan combinar un estilo urbano con un compromiso con el medio ambiente. Perfectas para quienes desean apoyar el movimiento de sostenibilidad sin sacrificar la calidad y el rendimiento.",
 		price: 70999,
 		stock: 6,
-		image: "../../public/ProductImage1.png",
+		image: "/ProductImage1.png",
 	},
 	{
 		id: 2,
@@ -17,7 +17,7 @@ export const dataProducts = [
 			"Los botines Adidas F50 League Jr están diseñados para jóvenes jugadores que buscan una gran agilidad en el campo. Su diseño ligero y ajuste cómodo brindan una excelente tracción y velocidad. Con estos botines, los más pequeños podrán mejorar su rendimiento mientras disfrutan de una mayor comodidad durante los partidos.",
 		price: 156000,
 		stock: 14,
-		image: "../../public/ProductImage2.png",
+		image: "/ProductImage2.png",
 	},
 	{
 		id: 3,
@@ -27,7 +27,7 @@ export const dataProducts = [
 			"Inspirados por el talento y la agilidad de Lionel Messi, están diseñados para ofrecer la máxima velocidad en el campo. Su tecnología avanzada garantiza un ajuste perfecto y un control superior del balón, permitiendo a los futbolistas moverse rápidamente con total precisión.",
 		price: 60000,
 		stock: 0,
-		image: "../../public/ProductImage3.png",
+		image: "/ProductImage3.png",
 	},
 	{
 		id: 4,
@@ -37,7 +37,7 @@ export const dataProducts = [
 			"Son el calzado perfecto para la temporada de descanso o para un estilo relajado. Con su diseño minimalista y una suela gruesa que ofrece confort y soporte, estas sandalias son ideales tanto para un look urbano casual como para disfrutar de la comodidad en casa. El diseño sencillo y elegante las convierte en una opción imprescindible para cualquier ocasión informal.",
 		price: 94000,
 		stock: 9,
-		image: "../../public/ProductImage4.png",
+		image: "/ProductImage4.png",
 	},
 	{
 		id: 5,
@@ -47,7 +47,7 @@ export const dataProducts = [
 			"Con un diseño innovador y futurista, están pensadas para quienes buscan comodidad sin perder el estilo. Su suela ondulada y su material flexible proporcionan un ajuste suave, mientras que su diseño único las convierte en una pieza esencial para quienes aman un look moderno y práctico. Perfectas para usarlas en cualquier lugar, desde la playa hasta la ciudad.",
 		price: 89999,
 		stock: 7,
-		image: "../../public/ProductImage5.png",
+		image: "/ProductImage5.png",
 	},
 	{
 		id: 6,
@@ -57,7 +57,7 @@ export const dataProducts = [
 			"Son una excelente opción para quienes buscan una zapatilla deportiva con un toque de lujo y rendimiento. Combinando materiales de alta calidad y un diseño único, estas zapatillas ofrecen un ajuste superior y un estilo impecable. Con una suela que brinda una gran tracción y un look clásico de la línea Jordan, son ideales tanto para actividades deportivas como para el día a día.",
 		price: 219999,
 		stock: 12,
-		image: "../../public/ProductImage6.png",
+		image: "/ProductImage6.png",
 	},
 	{
 		id: 7,
@@ -67,7 +67,7 @@ export const dataProducts = [
 			"Se trata de una interpretación moderna de un clásico de los 80. Su diseño alto y robusto se combina con detalles retro para ofrecer un estilo que nunca pasa de moda. Con una construcción duradera y un ajuste cómodo, estas zapatillas son perfectas para quienes buscan un toque de nostalgia y un alto rendimiento en el calzado diario.",
 		price: 165000,
 		stock: 7,
-		image: "../../public/ProductImage7.png",
+		image: "/ProductImage7.png",
 	},
 	{
 		id: 8,
@@ -77,7 +77,7 @@ export const dataProducts = [
 			"Diseñados para proporcionar un rendimiento excepcional en el campo. Con tecnología avanzada para mejorar la tracción y la estabilidad, estos botines son ideales para los futbolistas que buscan rapidez y agilidad en cada jugada. Su diseño aerodinámico y ajuste preciso permiten moverse con total libertad mientras controlas el balón con facilidad.",
 		price: 189999,
 		stock: 10,
-		image: "../../public/ProductImage8.png",
+		image: "/ProductImage8.png",
 	},
 	{
 		id: 9,
@@ -87,7 +87,7 @@ export const dataProducts = [
 			"Diseñados para jugadores que buscan comodidad y control en cada toque. Confeccionados con materiales de alta calidad, ofrecen un ajuste perfecto y una sensación excepcional al contacto con el balón. Su suela optimizada proporciona estabilidad y tracción en diferentes superficies, permitiendo un desempeño superior en el campo.",
 		price: 109999,
 		stock: 13,
-		image: "../../public/ProductImage9.png",
+		image: "/ProductImage9.png",
 	},
 	{
 		id: 10,
@@ -97,7 +97,7 @@ export const dataProducts = [
 			"La combinación perfecta de comodidad y estilo. Estas sandalias cuentan con una plantilla acolchonada que brinda una sensación suave y cómoda en cada paso. Su diseño ligero y resistente al agua las hace ideales para usar después del entrenamiento, en la piscina o en el día a día.",
 		price: 46000,
 		stock: 7,
-		image: "../../public/ProductImage10.png",
+		image: "/ProductImage10.png",
 	},
 	{
 		id: 11,
@@ -107,7 +107,7 @@ export const dataProducts = [
 			"Diseñadas para quienes buscan ligereza y amortiguación en cada zancada. Estas zapatillas cuentan con una entresuela de espuma que absorbe los impactos, proporcionando comodidad durante todo el día. Su diseño transpirable y su suela flexible las convierten en una opción ideal para entrenamientos y uso diario.",
 		price: 69999,
 		stock: 2,
-		image: "../../public/ProductImage11.png",
+		image: "/ProductImage11.png",
 	},
 	{
 		id: 12,
@@ -117,7 +117,7 @@ export const dataProducts = [
 			"Un equilibrio perfecto entre confort y rendimiento. Su diseño versátil y su construcción ligera las hacen ideales para entrenamientos dinámicos o el día a día. Con una suela flexible y una amortiguación responsiva, brindan estabilidad y soporte en cada movimiento.",
 		price: 75000,
 		stock: 4,
-		image: "../../public/ProductImage12.png",
+		image: "/ProductImage12.png",
 	},
 ];
 
